53b2e19c3f60807b9d36c965307a53ad5aeeccfa
Tanshi Khandelwal <tanshikhandelwal03@gmail.com>
2025-07-21 23:14:38 +0530

    Fix!!

--- hotel_managemen_system/settings.py
+++ hotel_managemen_system/settings.py
@@ -22,10 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3q*2-htly(ueewu=a^-5&*4c^^-)0oe5futujfhxae4!dnmv=g'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
